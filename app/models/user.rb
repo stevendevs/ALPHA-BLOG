41d1b5434892c6d500901cf850d0af1e27d761ab
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-04-20 14:19:28 -0600

    user information in index article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
     has_many :articles 
 
-validates :name, presence: true, length: {minimum: 3, maximum: 25}
+validates :name, 
+                presence: true, uniqueness: true ,
+                length: {minimum: 3, maximum: 25}
+validates :email, 
+                presence: true,
+                format: { with: URI::MailTo::EMAIL_REGEXP },
+                length: {minimum: 3, maximum: 25}
 
 end
